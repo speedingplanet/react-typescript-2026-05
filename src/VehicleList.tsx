d50de0359a1b5f3a5c1931449397a98ebb1eabe6
John Paxton <johnpaxton@gmail.com>
2026-05-20 16:26:18 -0400

    Updated VehicleList

--- src/VehicleList.tsx
+++ src/VehicleList.tsx
@@ -18,6 +18,88 @@
 	- intakeDate
 */
 
+import { useState } from 'react';
+import { orderBy } from 'lodash-es';
+import { vehicles } from './data/vehicles.json' with { type: 'json' };
+import type { Vehicle } from './index-types';
+
+export type SortDirection = 'asc' | 'desc';
+
+export interface SortConfig<T> {
+	sortField: keyof T | '';
+	sortDirection: SortDirection;
+}
+
+let initialSortConfig: SortConfig<Vehicle> = {
+	sortField: '',
+	sortDirection: 'asc',
+};
+
 export default function VehicleList() {
-	return <div>VehicleList</div>;
+	let [sortConfig, setSortConfig] = useState(initialSortConfig);
+
+	let columns: Array<keyof Vehicle> = [
+		'make',
+		'model',
+		'year',
+		'activeStatus',
+		'type',
+		'vin',
+		'intakeDate',
+	];
+
+	function handleColumnClick(column: keyof Vehicle) {
+		console.log(`Clicked on... ${column}`);
+		sortVehicles(column);
+	}
+
+	function sortVehicles(sortField: keyof Vehicle) {
+		let sortDirection: SortDirection = 'asc';
+		if (sortConfig.sortField === sortField && sortConfig.sortDirection === 'asc') {
+			sortDirection = 'desc';
+		}
+
+		let nextSortConfig: SortConfig<Vehicle> = {
+			sortField,
+			sortDirection,
+		};
+
+		setSortConfig(nextSortConfig);
+	}
+
+	function getSortIndicator(field: string, sortConfig: SortConfig<Vehicle>) {
+		if (field !== sortConfig.sortField) return;
+
+		return sortConfig.sortDirection === 'asc' ? '⬆️' : '⬇️';
+	}
+
+	let sortedVehicles = orderBy(vehicles, sortConfig.sortField, sortConfig.sortDirection);
+
+	return (
+		<table className="table table-striped">
+			<thead>
+				<tr>
+					{columns.map((column) => (
+						<th
+							key={column}
+							onClick={() => handleColumnClick(column)}
+						>
+							{column}
+							&nbsp;
+							<span>{getSortIndicator(column, sortConfig)}</span>
+						</th>
+					))}
+				</tr>
+			</thead>
+			<tbody>
+				{sortedVehicles.map((vehicle) => (
+					<tr key={vehicle.id}>
+						{columns.map((column) => (
+							<td key={column}>{vehicle[column as keyof Vehicle]}</td>
+						))}
+					</tr>
+				))}
+			</tbody>
+		</table>
+	);
 }
